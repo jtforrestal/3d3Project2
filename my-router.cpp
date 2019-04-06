0779eae4e625cb8b2a645a16f52eb402d206ba69
Jack Forrestal <jforrest@tcd.ie>
2019-04-06 20:19:15 +0100

    string

--- my-router.cpp
+++ my-router.cpp
@@ -486,7 +486,7 @@ int main(int argc, char *argv[])
                 }
                   std::cout<< recvd_message<< "\n";  // output what is left of the packet
                   recvd_message = "Type:DATA\nSrc_Node:"+ srcname +"\n" + destname + ":" +totalcost + recvd_message;
-                if ((rv = getaddrinfo("localhost", next_hop, &hints, &servinfo)) != 0) {
+                if ((rv = getaddrinfo("localhost", next_hop.c_str(), &hints, &servinfo)) != 0) {
                    fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
                    return 1;
                }
